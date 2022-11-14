d591dc6b3b4f87f28ed3b30eea3de028298740d5
Ida Andersson <andson_4id@hotmail.com>
2022-11-14 16:04:43 +0100

    Ida

--- score.js
+++ score.js
@@ -5,3 +5,37 @@ let result = sessionStorage.getItem('result')
 let points = document.querySelector('#points')
 
 points.textContent = result
+
+/* Play again */
+
+let playButton = document.querySelector('.play')
+
+playButton.addEventListener('mouseenter', () => {
+
+        let playAgain = document.querySelector('#play-again')
+        let scorePres = document.querySelector('#result-container > h2')
+        let questionmark = document.querySelector('#questionmark')
+        let oldPoints = document.querySelector('#result-container > p')
+
+        playAgain.style.display = 'block'
+        scorePres.style.display = 'none'
+
+        questionmark.style.display = 'block'
+        oldPoints.style.display = 'none'
+
+})
+
+playButton.addEventListener('mouseleave', () => {
+
+        let playAgain = document.querySelector('#play-again')
+        let scorePres = document.querySelector('#result-container > h2')
+        let questionmark = document.querySelector('#questionmark')
+        let oldPoints = document.querySelector('#result-container > p')
+
+        playAgain.style.display = 'none'
+        scorePres.style.display = 'block'
+
+        questionmark.style.display = 'none'
+        oldPoints.style.display = 'block'
+
+})
